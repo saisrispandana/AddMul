1153205abbf4391513864226e85f5ce6e11dafb2
SpandanaTest <saisrispandana256@gmail.com>
2021-03-19 22:29:33 +0530

    gjhjgh

---
+++ Num_Pro.java
@@ -0,0 +1,19 @@
+
+public class Num_Pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=2,b=3,c;
+        c=a+b;
+        System.out.println("C value is:"+c);
+		}
+		
+		
+		
+		
+		
+				
+		
+			
+	}
+
